5633366a56cae470f6b6a47c90d7910084ca29c7
Toshiya Kawasaki <toshiya@abeja.asia>
2017-07-12 11:20:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+Datadog custom agent check for http authorized with JWT
+===
+Http checks of http endpoints authorized with JWT.
+
+This plugin collects:
+- `${metrics_prefix}.http.request.success` ( `1` for request success and `0` for request failure)
+- `${metrics_prefix}.http.request.latency`
+
+# Installation
+
+On the agent host.
+- Copy `checks.d/jwt_http_check.py` to `checks.d`.
+- Copy `conf.d/jwt_http_check.yaml` to `conf.d` and write config.
+
+*You can check the path of `checks.d` and `conf.d` by typing `sudo dd-agent info`.*
+
+Then,
+```
+$ sudo service datadog-agent reload
+```
+
+
+# Configuration
+
+```
+init_config:
+  default_timeout: 5
+
+instances:
+  - url: https://example.com/cars
+    method: POST
+    headers:
+      Content-Type: image/jpeg
+    data_image_url: https://example.com/test.jpg
+    tags:
+      - app:example-api
+      - env:dev
+      - endpoint:cars
+    jwt:
+      iss: xxx
+      aud: yyy
+      kid: kkk
+      algorithm: RS256
+      secret_key: |
+        -----BEGIN RSA PRIVATE KEY-----
+        .........
+        -----END RSA PRIVATE KEY-----
+
+  - url: https://example.com/drivers
+    method: GET
+    tags:
+      - app:example-api
+      - env:dev
+      - endpoint:drivers
+    jwt:
+      iss: xxx
+      aud: yyy
+      kid: kkk
+      algorithm: RS256
+      secret_key: |
+        -----BEGIN RSA PRIVATE KEY-----
+        .........
+        -----END RSA PRIVATE KEY-----
+```
+
+|Setting |Description|
+|---|---|
+|url | the url to test|
+|method | The HTTP method. This setting defaults to GET, though many other HTTP methods are supported, including POST and PUT.  |
+|timeout | The time in seconds to allow for a response.|
+|headers | http headers|
+| http_response_status_code|A string or Python regular expression for an HTTP status code. This check will report DOWN for any status code that does not match. This defaults to 1xx, 2xx and 3xx HTTP status codes. For example: 401 or 4\d\d.|
+| data_image_url | image data will be sent in the body of the request. data is ignored if data_image_url is specified|
+| data | Data should be included as key-value pairs and will be sent in the body of the request as json|
+| metrics_prefix | prefix of metrics |
+| jwt.iss | jwt param to get token |
+| jwt.aud  |  jwt param to get token |
+| jwt.kid  |  jwt param to get token |
+| jwt.algorithm  |  jwt param to get token |
+| jwt.ttl_in_second  |  jwt param to get token, default value is 86400 |
+| jwt.secret_key  |  jwt param to get token |
---
+++ checks.d/jwt_http_check.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+from checks import AgentCheck
+import requests
+import os
+import time
+import json
+import jwt
+import re
+
+
+class JwtHttpCheck(AgentCheck):
+    def get_token(self, iss, aud, kid, secret_key, algorithm, ttl_in_second):
+        now = time.time()
+        td = now + ttl_in_second
+        payload = {
+            "iss": iss,
+            "aud": aud,
+            "iat": str(int(now)),
+            "exp": str(int(td))
+        }
+        encoded = jwt.encode(payload, secret_key, algorithm, headers={'kid': kid})
+        scheme = 'Bearer'
+        authorization_token = '{} {}'.format(scheme, encoded.decode('utf-8'))
+        return authorization_token
+
+    def get_image(self, url):
+        filename = os.path.basename(url)
+        filepath = os.path.join('/tmp', filename)
+        if not os.path.exists(filepath):
+            r = requests.get(url)
+            with open(filepath, 'wb') as f:
+                f.write(r.content)
+        with open(filepath, 'rb') as f:
+            return f.read()
+
+    def check(self, instance):
+        url = instance['url']
+        method = instance['method']
+        headers = instance.get('headers', {})
+        data = instance.get('data', {})
+        metrics_prefix = instance.get('metrics_prefix', 'jwt')
+        data_image_url = instance.get('data_image_url', None)
+        iss = instance['jwt']['iss']
+        aud = instance['jwt']['aud']
+        ttl_in_second = int(instance['jwt'].get('ttl_in_second', (24 * 60 * 60))
+        kid = instance['jwt']['kid']
+        algo = instance['jwt']['algorithm']
+        secret_key = instance['jwt']['secret_key']
+        tags = instance['tags']
+        http_response_status_code = instance.get('http_response_status_code', '2\d\d')
+        default_timeout = self.init_config.get('default_timeout', 5)
+        timeout = float(instance.get('timeout', default_timeout))
+
+        token = self.get_token(iss, aud, kid, secret_key, ttl_in_second)
+        headers = {'Authorization': token}
+
+        if data_image_url is not None:
+            _data = self.get_image(data_image_url)
+        else:
+            _data=json.dumps(data)
+        s = time.time()
+        try:
+            r = requests.request(method, url, headers=headers, data=_data, timeout=timeout)
+        except:
+            success = 0
+        else:
+            if not re.match(http_response_status_code, str(r.status_code)):
+                success = 0
+            else:
+                success = 1
+        e = time.time()
+        latency = int((e - s) * 1000)
+        self.gauge('{}.http.request.success'.format(metrics_prefix), success, tags=tags)
+        self.gauge('{}.http.request.latency'.format(metrics_prefix), latency, tags=tags)
+
+
+if __name__ == '__main__':
+    check, instances = ArmsHTTPCheck.from_yaml('/opt/datadog-agent/etc/conf.d/custom_http.yaml')
+    for instance in instances:
+        check.check(instance)
+        if check.has_events():
+            print(check.get_events())
+        print(check.get_metrics())
